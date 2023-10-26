21c31b6b144147e998ba98ed2242d0c7c6bf9615
liguanghao <liguanghao@viatomtech.com>
2023-10-26 18:15:48 +0800

    dfsdbnjfdgvb

--- app/src/main/java/com/vaca/accessibility_android/MyService.kt
+++ app/src/main/java/com/vaca/accessibility_android/MyService.kt
@@ -14,6 +14,7 @@ import android.util.Log
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
 import androidx.annotation.RequiresApi
+import com.vaca.accessibility_android.MyService.Companion.clickByNode
 
 
 class MyService : AccessibilityService() {
@@ -54,7 +55,7 @@ class MyService : AccessibilityService() {
                     list6 = rootNodeInfo.findAccessibilityNodeInfosByText("用户")
                     if (null != list6) {
                         for (info in list6!!) {
-                       //     Log.e(TAG, info.toString())
+                            Log.e(TAG, info.toString())
                             clickByNode(this, info.parent)
                         }
                     }
