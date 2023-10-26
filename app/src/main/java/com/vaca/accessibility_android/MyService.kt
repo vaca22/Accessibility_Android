656693ceda80ac3596ee2d7b7e14269942d9b47f
liguanghao <liguanghao@viatomtech.com>
2023-10-26 18:11:40 +0800

    dfsdbnjfdg

--- app/src/main/java/com/vaca/accessibility_android/MyService.kt
+++ app/src/main/java/com/vaca/accessibility_android/MyService.kt
@@ -46,126 +46,10 @@ class MyService : AccessibilityService() {
         val packageName = event.packageName.toString()
         val className = event.className.toString()
         when (eventType) {
-            AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED -> {
-               // Log.e(TAG, "AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED跳转到新的页面")
-                if (className == "com.ss.android.ugc.aweme.main.MainActivity") {
-                    list8 =
-                        rootNodeInfo.findAccessibilityNodeInfosByViewId("com.ss.android.ugc.aweme:id/a-5")
-                    if (null != list8) {
-                        for (info in list8!!) {
-                            info.performAction(AccessibilityNodeInfo.ACTION_CLICK)
-                        }
-                    }
-                    list2 =
-                        rootNodeInfo.findAccessibilityNodeInfosByViewId("com.ss.android.ugc.aweme:id/hox")
-                    if (null != list2) {
-                        for (info in list2!!) {
-//                            Log.e(TAG, info.toString())
-                            info.parent.parent.performAction(AccessibilityNodeInfo.ACTION_CLICK)
-                        }
-                    }
-                }
-                if (className == "com.lynx.tasm.behavior.KeyboardMonitor") {
-                    Log.e(TAG, "执行了输入框")
-                    list3 =
-                        rootNodeInfo.findAccessibilityNodeInfosByViewId("com.ss.android.ugc.aweme:id/et_search_kw")
-                    if (null != list3) {
-                        for (nodeInfo in list3!!) {
-                            val arguments = Bundle()
-                            arguments.putCharSequence(
-                                AccessibilityNodeInfo.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE,
-                                "111111"
-                            )
-                            nodeInfo.performAction(
-                                AccessibilityNodeInfo.ACTION_SET_TEXT,
-                                arguments
-                            )
-                        }
-                    }
-                }
-               // Log.e(TAG, "AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED某个view的内容发生的变化")
-                Log.e(TAG, "onServiceConnected:" + "实现辅助功能")
-                Log.d("vaca", "packageName = $packageName, className = $className")
-                if (className == "com.lynx.tasm.behavior.KeyboardMonitor") {
-                    Log.e(TAG, "执行了搜索按钮")
-                    list5 =
-                        rootNodeInfo.findAccessibilityNodeInfosByViewId("com.ss.android.ugc.aweme:id/rzy")
-                    if (null != list5) {
-                        for (info in list5!!) {
-                            clickByNode(this, info)
-                        }
-                    }
-                }
-                if (className == "androidx.recyclerview.widget.RecyclerView") {
-                    list6 = rootNodeInfo.findAccessibilityNodeInfosByText("用户")
-                    if (null != list6) {
-                        for (info in list6!!) {
-                            //Log.e(TAG, info.toString())
-                            clickByNode(this, info.parent)
-                        }
-                    }
-                    list7 = rootNodeInfo.findAccessibilityNodeInfosByText("关注")
-                    if (null != list7) {
-                        for (info in list7!!) {
-                           // Log.e(TAG, info.toString())
-                        }
-                    }
-                }
-                val rootNode = rootInActiveWindow
-                //匹配Text获取节点
-                val list1 = rootNode.findAccessibilityNodeInfosByText("match_text")
-                //匹配id获取节点
-                val list2 = rootNode.findAccessibilityNodeInfosByViewId("match_id")
-                //获取子节点
-//                val infoNode = rootNode.getChild(index)
-            }
-            AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED -> {
-//                Log.e(TAG, "AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED某个view的内容发生的变化")
-//                Log.e(TAG, "onServiceConnected:" + "实现辅助功能")
-                Log.d("TAG", "packageName = $packageName, className = $className")
-                if (className == "com.lynx.tasm.behavior.KeyboardMonitor") {
-                    Log.e(TAG, "执行了搜索按钮")
-                    list5 =
-                        rootNodeInfo.findAccessibilityNodeInfosByViewId("com.ss.android.ugc.aweme:id/rzy")
-                    if (null != list5) {
-                        for (info in list5!!) {
-                            clickByNode(this, info)
-                        }
-                    }
-                }
-                if (className == "androidx.recyclerview.widget.RecyclerView") {
-                    list6 = rootNodeInfo.findAccessibilityNodeInfosByText("用户")
-                    if (null != list6) {
-                        for (info in list6!!) {
-                          //  Log.e(TAG, info.toString())
-                            clickByNode(this, info.parent)
-                        }
-                    }
-                    list7 = rootNodeInfo.findAccessibilityNodeInfosByText("关注")
-                    if (null != list7) {
-                        for (info in list7!!) {
-                       //     Log.e(TAG, info.toString())
-                        }
-                    }
-                }
-                val rootNode = rootInActiveWindow
-                val list1 = rootNode.findAccessibilityNodeInfosByText("match_text")
-                val list2 = rootNode.findAccessibilityNodeInfosByViewId("match_id")
-//                val infoNode = rootNode.getChild(index)
-            }
+
             AccessibilityEvent.TYPE_VIEW_SCROLLED -> {
-                Log.e(TAG, "onServiceConnected:" + "实现辅助功能")
+                Log.e(TAG, "TYPE_VIEW_SCROLLED")
                 Log.d("TAG", "packageName = $packageName, className = $className")
-                if (className == "com.lynx.tasm.behavior.KeyboardMonitor") {
-                    Log.e(TAG, "执行了搜索按钮")
-                    list5 =
-                        rootNodeInfo.findAccessibilityNodeInfosByViewId("com.ss.android.ugc.aweme:id/rzy")
-                    if (null != list5) {
-                        for (info in list5!!) {
-                            clickByNode(this, info)
-                        }
-                    }
-                }
                 if (className == "androidx.recyclerview.widget.RecyclerView") {
                     list6 = rootNodeInfo.findAccessibilityNodeInfosByText("用户")
                     if (null != list6) {
@@ -181,15 +65,6 @@ class MyService : AccessibilityService() {
                         }
                     }
                 }
-                val rootNode = rootInActiveWindow
-                val list1 = rootNode.findAccessibilityNodeInfosByText("match_text")
-                val list2 = rootNode.findAccessibilityNodeInfosByViewId("match_id")
-                //log all node info
-                //log all node info
-//                for(i in 0 until rootNode.childCount){
-//                    Log.e(TAG, "onAccessibilityEvent: " + rootNode.getChild(i).toString())
-//                }
-//                val infoNode = rootNode.getChild(index)
             }
         }
     }
